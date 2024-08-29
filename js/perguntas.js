f5e74e5122702cc54e813598ad0be6d3de9efca8
grazziakelly <108533593+grazziakelly@users.noreply.github.com>
2024-08-29 11:19:37 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,169 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é a principal importância da faculdade na formação de um indivíduo?",
+        alternativas: [
+            {
+                texto: " Oferecer um ambiente para aprofundar conhecimentos e desenvolver habilidades profissionais.",
+                afirmacao: [
+                    "Preparação mais robusta para o mercado de trabalho, com habilidades práticas e conhecimento especializado.",
+                    "Ampliação das oportunidades de carreira e maior capacidade de adaptação às mudanças da indústria."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Proporcionar uma pausa temporária antes de entrar diretamente no mercado de trabalho.",
+                afirmacao: [
+                    "Pode levar a uma falta de experiência prática imediata, dificultando a transição para o mercado de trabalho.",
+                    "Possível atraso na entrada em uma carreira profissional, que pode afetar a trajetória financeira e profissional a curto prazo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual fator é crucial para maximizar os benefícios de uma experiência universitária?",
+        alternativas: [
+            {
+                texto: "Escolher um curso que alinha com suas paixões e interesses pessoais.",
+                afirmacao: [
+                    "Maior motivação e engajamento nas atividades acadêmicas, resultando em um desempenho superior.",
+                    "Desenvolvimento de uma carreira mais satisfatória e gratificante, com maior potencial de sucesso e realização pessoal."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Participar exclusivamente de atividades extracurriculares e evitar focar nas disciplinas acadêmicas.",
+                afirmacao: [
+                    "Possível defasagem no conhecimento acadêmico essencial, o que pode comprometer a formação profissional.",
+                    "Dificuldades em obter um diploma ou certificação, limitando as oportunidades de emprego na área desejada."     
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um dos maiores desafios enfrentados pelos estudantes universitários?",
+        alternativas: [
+            {
+                texto: "Equilibrar as demandas acadêmicas com as responsabilidades pessoais e profissionais.",
+                afirmacao: [
+                    "Estresse e Sobrecarregamento: O desafio de conciliar estudos, trabalho e vida pessoal pode levar ao estresse e ao esgotamento, afetando a saúde e o desempenho acadêmico.                    ",
+                    "Desempenho Acadêmico Comprometido: Dificuldades em manter o equilíbrio podem resultar em um desempenho acadêmico abaixo do esperado, prejudicando a qualidade do aprendizado e a conclusão do curso."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Encontrar oportunidades para viajar e explorar novas culturas.",
+                afirmacao: [
+                    "Enriquecimento Cultural e Pessoal: Viajar e conhecer novas culturas pode proporcionar experiências valiosas que ampliam a perspectiva global e a compreensão intercultural.",
+                    "Possível Desvio do Foco Acadêmico: Focar excessivamente em viagens e experiências extracurriculares pode desviar a atenção das responsabilidades acadêmicas, impactando negativamente o progresso no curso."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um benefício significativo de participar de estágios durante a faculdade?",
+        alternativas: [
+            {
+                texto: "Ganhar experiência prática relevante e criar conexões profissionais valiosas.",
+                afirmacao: [
+                    "Aumento da Empregabilidade: A experiência prática e as conexões profissionais podem facilitar a transição para o mercado de trabalho e aumentar as chances de conseguir um emprego após a graduação.",
+                    "Desenvolvimento de Habilidades: Estágios proporcionam a oportunidade de aplicar conhecimentos acadêmicos em situações reais, aprimorando habilidades técnicas e interpessoais essenciais para a carreira."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Ter mais tempo livre para participar de atividades sociais e eventos extracurriculares.",
+                afirmacao: [
+                    "Menos Experiência Prática: A falta de estágios pode resultar em menos experiência prática, o que pode dificultar a obtenção de um emprego na área desejada após a graduação.",
+                    "Possível Falta de Preparação Profissional: A ênfase excessiva em atividades sociais pode limitar o desenvolvimento de habilidades profissionais e a construção de uma rede de contatos importantes para a carreira."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um benefício importante de buscar orientação de professores e mentores durante a faculdade?",
+        alternativas: [
+            {
+                texto: "Receber conselhos e apoio que ajudam a tomar decisões informadas sobre a carreira e os estudos.",
+                afirmacao: [
+                    "Orientação Profissional: Obter direcionamento pode facilitar a escolha de um caminho de carreira alinhado com seus interesses e habilidades, aumentando as chances de sucesso profissional.",
+                    "Desenvolvimento Pessoal: O apoio de mentores e professores pode promover crescimento acadêmico e pessoal, ajudando a superar desafios e alcançar metas mais ambiciosas.                    "
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Garantir mais tempo livre para se dedicar a hobbies e interesses pessoais.",
+                afirmacao: [
+                    "Possível Falta de Orientação: Menos interação com mentores pode resultar em falta de direção e suporte nas escolhas acadêmicas e profissionais.",
+                    "Desvio dos Objetivos Acadêmicos: Focar excessivamente em hobbies pode levar a uma menor dedicação aos estudos, impactando negativamente o desempenho acadêmico e as oportunidades futuras."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como escolher a faculdade certa para suas necessidades e objetivos de carreira?",
+        alternativas: [
+            {
+                texto: "Considerar a reputação do curso e a qualidade do corpo docente em sua área de interesse.                ",
+                afirmacao: [
+                    "Formação de Alta Qualidade: A escolha de uma faculdade com boa reputação e corpo docente qualificado pode garantir uma educação mais robusta e relevante para sua carreira.                    .",
+                    "Oportunidades de Networking: Instituições renomadas frequentemente oferecem melhores oportunidades de networking e conexões profissionais, facilitando o acesso a estágios e empregos.                    "
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Optar pela faculdade mais próxima de casa para economizar tempo e despesas com deslocamento.",
+                afirmacao: [
+                    "Menos Oportunidades de Especialização: A proximidade pode limitar a escolha de cursos e programas de alta qualidade ou especializados que atendem melhor suas ambições profissionais.                    ",
+                    "Possível Menor Experiência Acadêmica: A faculdade próxima pode não oferecer o mesmo nível de recursos e suporte acadêmico, afetando o desenvolvimento profissional e acadêmico.                    "
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual fator deve ser considerado ao decidir entre uma faculdade pública e uma faculdade privada?",
+        alternativas: [
+            {
+                texto: "A adequação do currículo e das oportunidades de estágio oferecidos para a sua área de estudo e interesses profissionais.",
+                afirmacao: [
+                    "Formação Alinhada com Carreira: Escolher uma faculdade que oferece um currículo robusto e boas oportunidades de estágio pode garantir uma preparação mais completa e relevante para sua futura profissão.",
+                    "Maior Potencial de Empregabilidade: Programas bem estruturados e estágios de qualidade aumentam as chances de conseguir um emprego na área desejada após a graduação.                    "
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "O número de atividades extracurriculares disponíveis para entretenimento e lazer.",
+                afirmacao: [
+                    "Desvio do Foco Acadêmico: Priorizar atividades extracurriculares pode levar a menos tempo dedicado aos estudos e à formação acadêmica, impactando negativamente o desempenho e a preparação profissional.                    ",
+                    "Menos Enfoque na Qualidade do Ensino: Focar em atividades de lazer pode fazer com que se perca de vista a importância de um currículo acadêmico sólido e oportunidades práticas relacionadas à carreira."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão,Qual é uma consideração importante ao avaliar o custo de uma faculdade?E agora?",
+        alternativas: [
+            {
+                texto: "O retorno sobre o investimento em termos de potencial salarial e oportunidades de carreira após a graduação.",
+                afirmacao: [
+                    "Valorização da Educação: Avaliar o retorno sobre o investimento ajuda a garantir que o custo da faculdade seja compensado por um aumento significativo no potencial salarial e nas oportunidades de carreira.",
+                    "Planejamento Financeiro: Esta consideração permite tomar decisões informadas sobre financiamento e endividamento, ajudando a garantir que o custo da faculdade seja sustentável a longo prazo."
+                ],
+            },
+            {
+                texto: "A quantidade de eventos sociais e atividades recreativas oferecidas pelo campus.",
+                afirmacao: [
+                    "Menos Foco no Investimento: Priorizar eventos sociais pode desviar a atenção da importância de avaliar o custo em relação ao retorno financeiro e profissional, possivelmente resultando em uma escolha menos estratégica.",
+                    "Desconexão com Benefícios Acadêmicos: Focar apenas nas atividades recreativas pode negligenciar aspectos essenciais do custo que impactam diretamente a formação acadêmica e as oportunidades de carreira.",
+                ]
+
+            },
+        ]
+    }
+];
